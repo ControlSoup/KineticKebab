1c72ada6ab3593394b1947c0989362520962e774
Joe Wilson <your_email@example.com>
2023-09-03 17:07:52 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
+# Auto generate install reqs
+
+with open('requirements.txt','r') as f:
+    REQ_LINES = list(f.readlines()) 
+
+for i in REQ_LINES:
+    i += ','
+
 setup(
     name='KineticKebab',
     version='0.0.1',
@@ -8,8 +16,5 @@ setup(
     author_email='joe.burge.iii@gmail.com',
     description='Engineering analysis repository for personal projects',
     packages=find_packages(),    
-    install_requires=[
-        'numpy >= 1.11.1',
-        'matplotlib >= 1.5.1'
-    ],
+    install_requires=REQ_LINES,
 )
